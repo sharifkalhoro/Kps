35c5644851ed25c3d824943504a9a791122d89b0
sharifkalhoro <sharifuddinabbasi@gmail.com>
2026-09-10 11:20:07 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Handles background request fetching
+});
+
+
